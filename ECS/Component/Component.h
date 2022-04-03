04b37f69eaad7daa1383a41e4c182b818fe86a91
Rhidian De Wit <74491212+Rhidian12@users.noreply.github.com>
2022-04-03 15:19:26 +0200

    finished base component class

--- ECS/Component/Component.h
+++ ECS/Component/Component.h
@@ -1,8 +1,17 @@
 #pragma once
+#include "../ECSConstants.h"
 
 namespace ECS
 {
 	class Component
 	{
+	public:
+		virtual ~Component() = default;
+
+		__forceinline auto GetComponentID() const noexcept { return ComponentID; }
+
+	protected:
+		/* we need a way to differentiate components from one another, and I refuse to use typeid() [cus it's fucking cringe] */
+		ComponentType ComponentID;
 	};
 }
