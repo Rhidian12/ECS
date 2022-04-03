f77fd94e8cf436f4c54d1941e8320d6a37b96964
Rhidian De Wit <74491212+Rhidian12@users.noreply.github.com>
2022-04-03 17:26:56 +0200

    added SystemID to System

--- ECS/System/System.h
+++ ECS/System/System.h
@@ -1,7 +1,8 @@
 #pragma once
 #include "../ECSConstants.h"
 
-#include <unordered_set>
+#include <concepts> /* Concepts */
+#include <unordered_set> /* std::unordered_set */
 
 namespace ECS
 {
@@ -11,6 +12,10 @@ namespace ECS
 		virtual ~ISystem() = default;
 	};
 
+	template<typename Type>
+	concept IsDerivedSystem = std::is_base_of_v<class System, Type>;
+
+	template<IsDerivedSystem DerivedSystem>
 	class System
 	{
 	public:
@@ -21,7 +26,11 @@ namespace ECS
 
 		virtual void UpdateSystem() = 0; /* This function will never be called from a System*, it just enforces inheritance */
 
+		__forceinline auto GetSystemID() const noexcept { return SystemID; }
+
 	protected:
+		const SystemID SystemID{ FamilyTypeID<ISystem>::Get<DerivedSystem>() };
+
 		std::unordered_set<Entity> Entities;
 	};
 }
