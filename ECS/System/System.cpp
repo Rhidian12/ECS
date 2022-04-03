e59c00b4939f992ed0c119bb0f1879cb585b1b81
Rhidian De Wit <74491212+Rhidian12@users.noreply.github.com>
2022-04-03 17:20:44 +0200

    system finished for now

--- ECS/System/System.cpp
+++ ECS/System/System.cpp
@@ -2,5 +2,13 @@
 
 namespace ECS
 {
+	void System::AddEntity(const Entity id)
+	{
+		Entities.insert(id);
+	}
 
+	void System::RemoveEntity(const Entity id) noexcept
+	{
+		Entities.erase(id);
+	}
 }
--- ECS/System/System.h
+++ ECS/System/System.h
@@ -5,12 +5,21 @@
 
 namespace ECS
 {
+	class ISystem
+	{
+	public:
+		virtual ~ISystem() = default;
+	};
+
 	class System
 	{
 	public:
 		virtual ~System() = default;
 
-		virtual void UpdateSystem() = 0;
+		void AddEntity(const Entity id) noexcept;
+		void RemoveEntity(const Entity id) noexcept;
+
+		virtual void UpdateSystem() = 0; /* This function will never be called from a System*, it just enforces inheritance */
 
 	protected:
 		std::unordered_set<Entity> Entities;
