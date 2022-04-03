0b4952b898c6ac25cab552c2b94bf6846e70d3e3
Rhidian De Wit <74491212+Rhidian12@users.noreply.github.com>
2022-04-03 15:04:37 +0200

    made some name changes

--- ECS/ECSConstants.h
+++ ECS/ECSConstants.h
@@ -5,12 +5,12 @@
 
 namespace ECS
 {
-	using Entity = uint32_t; /* This already supports 4,294,967,295 possible entities, seems like enough */
-	using ComponentType = uint16_t; /* This supports 65535 possible component types, seems like enough */
+	using Entity = uint32_t; /* This supports 4,294,967,295 possible entities, seems like enough */
+	using ComponentType = uint16_t; /* This supports 65,535 possible component types, seems like enough */
 
 // #undef max /* This always causes issues with anythin std::<>::max() related */
 	constexpr ComponentType MaxComponentTypes{ std::numeric_limits<ComponentType>::max() };
-	constexpr Entity MaxEntityTypes{ std::numeric_limits<Entity>::max() };
+	constexpr Entity MaxEntities{ std::numeric_limits<Entity>::max() };
 // #define max(a,b) (((a) > (b)) ? (a) : (b))
 
 	using EntitySignature = std::bitset<MaxComponentTypes>;
