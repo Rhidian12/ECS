c1a25565c3d3b23271625ea16a8e2034d69f21ec
Rhidian De Wit <74491212+Rhidian12@users.noreply.github.com>
2022-04-03 16:32:24 +0200

    made ComponentTypeCounter

--- ECS/ComponentTypeCounter/ComponentTypeCounter.h
+++ ECS/ComponentTypeCounter/ComponentTypeCounter.h
@@ -3,7 +3,18 @@
 
 namespace ECS
 {
+	/* This class was made by the absolute genius Arne Van Kerschaver */
+	template <class T>
 	class ComponentTypeCounter
 	{
+	public:
+		/* Since it's ++Counter, the Component Counter will always start at 1, can be easily changed, but who cares */
+		template <typename U>
+		__forceinline static ComponentType Get() { return ++Counter; }
+
+		__forceinline static ComponentType Get() { return Counter; }
+
+	private:
+		inline static ComponentType Counter{};
 	};
 }
