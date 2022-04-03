58deca44f99fafeb20f6675be22652faab0deac3
Rhidian De Wit <74491212+Rhidian12@users.noreply.github.com>
2022-04-04 00:07:55 +0200

    finalied testing

--- ECS/ECS.cpp
+++ ECS/ECS.cpp
@@ -10,7 +10,14 @@
 #include "Component/Component.h"
 #include "SystemManager/SystemManager.h"
 
+#include "GOComponent/GOComponent.h"
+#include "GameObject/GameObject.h"
+
 #include <chrono>
+#include <deque>
+#include <algorithm>
+#include <numeric>
+#include <vector>
 
 __forceinline float RandomFloat(float min, float max)
 {
@@ -83,9 +90,52 @@ public:
 	ECS::ComponentManager* pComponentManager;
 };
 
+class GOGravityComponent final : public GO::Component
+{
+public:
+	float Gravity{ -981.f };
+};
+
+class GORigidBodyComponent final : public GO::Component
+{
+public:
+	GORigidBodyComponent(GOGravityComponent* const pGravityComponent)
+		: pGravityComponent{ pGravityComponent }
+	{}
+
+	virtual void Update() override
+	{
+		Velocity.y += pGravityComponent->Gravity * Mass;
+	}
+
+	float Mass{ RandomFloat(0.f, 100.f) };
+	Point2f Velocity;
+	GOGravityComponent* pGravityComponent;
+};
+
+class GOTransformComponent final : public GO::Component
+{
+public:
+	GOTransformComponent(GORigidBodyComponent* const pRigidbodyComponent)
+		: pRigidbodyComponent{ pRigidbodyComponent }
+	{}
+
+	virtual void Update() override
+	{
+		Position.x += pRigidbodyComponent->Velocity.x;
+		Position.y += pRigidbodyComponent->Velocity.y;
+	}
+
+	GORigidBodyComponent* pRigidbodyComponent;
+	Point2f Position{ Point2f::CreateRandomPoint2f(0.f, 1000.f) };
+	Point2f Rotation{ Point2f::CreateRandomPoint2f(0.f, 360.f) };
+	Point2f Scale{ Point2f::CreateRandomPoint2f(0.f, 3.f) };
+};
+
 int main(int*, char* [])
 {
 	using namespace ECS;
+	using namespace GO;
 
 	constexpr int Iterations{ ECS::MaxEntities };
 
@@ -97,6 +147,11 @@ int main(int*, char* [])
 
 	pSystem->pEntityManager = pEntityManager;
 	pSystem->pComponentManager = pComponentManager;
+
+	std::vector<GameObject*> GameObjects;
+
+	std::deque<long long> ECSTimes{};
+	std::deque<long long> GOTimes{};
 
 	for (int i{}; i < Iterations; ++i)
 	{
@@ -111,15 +166,62 @@ int main(int*, char* [])
 		pEntityManager->SetSignatureSafe(entity, GravityComponent::GetComponentID());
 
 		pSystem->AddEntity(entity);
+
+		GameObject* pG{ new GameObject{} };
+
+		pG->AddComponent(new GOGravityComponent{});
+		pG->AddComponent(new GORigidBodyComponent{ pG->GetComponent<GOGravityComponent>() });
+		pG->AddComponent(new GOTransformComponent{ pG->GetComponent<GORigidBodyComponent>() });
+
+		GameObjects.push_back(pG);
+	}
+
+
+	for (int i{}; i < Iterations; ++i)
+	{
+		std::chrono::steady_clock::time_point t1{ std::chrono::steady_clock::now() };
+
+		pSystem->UpdateSystem();
+
+		std::chrono::steady_clock::time_point t2{ std::chrono::steady_clock::now() };
+
+		ECSTimes.push_back(std::chrono::duration_cast<std::chrono::nanoseconds>(t2 - t1).count());
 	}
 
 	for (int i{}; i < Iterations; ++i)
 	{
-		pSystem->UpdateSystem();
+		std::chrono::steady_clock::time_point t1{ std::chrono::steady_clock::now() };
+
+		for (GameObject* const pG : GameObjects)
+		{
+			pG->Update();
+		}
+
+		std::chrono::steady_clock::time_point t2{ std::chrono::steady_clock::now() };
+
+		GOTimes.push_back(std::chrono::duration_cast<std::chrono::nanoseconds>(t2 - t1).count());
 	}
+
+	std::sort(ECSTimes.begin(), ECSTimes.end());
+	std::sort(GOTimes.begin(), GOTimes.end());
+
+	for (int i{}; i < Iterations / 10; ++i)
+	{
+		ECSTimes.pop_back();
+		ECSTimes.pop_front();
+
+		GOTimes.pop_back();
+		GOTimes.pop_front();
+	}
+
+	std::cout << "ECS Average: " << std::accumulate(ECSTimes.begin(), ECSTimes.end(), 0.f) << "\n";
+	std::cout << "GO Average: " << std::accumulate(GOTimes.begin(), GOTimes.end(), 0.f) << "\n";
 
 	delete pEntityManager;
 	delete pComponentManager;
+
+	for (GameObject* pG : GameObjects)
+		delete pG;
 
 	return 0;
 }
