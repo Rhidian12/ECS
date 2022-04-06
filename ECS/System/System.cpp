28fb484ca6593caca0df1447ee26045af7a12ef4
Rhidian De Wit <74491212+Rhidian12@users.noreply.github.com>
2022-04-06 21:54:36 +0200

    cleaned up code

--- ECS/System/System.cpp
+++ ECS/System/System.cpp
@@ -2,4 +2,25 @@
 
 namespace ECS
 {
+	System::~System()
+	{
+		for (ComponentInfo& info : Components)
+		{
+			for (IComponent*& pC : info.Components)
+			{
+				delete pC;
+				pC = nullptr;
+			}
+		}
+	}
+
+
+#ifdef DEBUG
+	Entity System::CreateEntity()
+	{
+		assert(static_cast<Entity>(Entities.size()) < MaxEntities);
+
+		return Entities.emplace_back(static_cast<Entity>(Entities.size()));
+	}
+#endif
 }
--- ECS/System/System.h
+++ ECS/System/System.h
@@ -10,120 +10,41 @@
 
 namespace ECS
 {
-	//class ISystem
-	//{
-	//public:
-	//	virtual ~ISystem() = default;
-	//};
-
-	//template<typename DerivedSystem>
-	//class System : public ISystem
-	//{
-	//public:
-	//	System();
-	//	virtual ~System() = default;
-
-	//	void AddEntity(const Entity id) noexcept;
-	//	void RemoveEntity(const Entity id) noexcept;
-
-	//	static __forceinline auto GetSystemID() noexcept { return SystemID; }
-
-	//protected:
-	//	inline static const SystemID SystemID{ TypeCounter<ISystem>::Get<DerivedSystem>() };
-
-	//	std::array<Entity, MaxEntities> Entities;
-	//};
-
-	//template<typename DerivedSystem>
-	//System<DerivedSystem>::System()
-	//	: Entities{}
-	//{
-	//	for (Entity& entity : Entities)
-	//	{
-	//		entity = InvalidEntityID;
-	//	}
-	//}
-
-	//template<typename DerivedSystem>
-	//void System<DerivedSystem>::AddEntity(const Entity id) noexcept
-	//{
-	//	Entities[id] = id;
-	//}
-
-	//template<typename DerivedSystem>
-	//inline void System<DerivedSystem>::RemoveEntity(const Entity id) noexcept
-	//{
-	//	Entities[id] = InvalidEntityID;
-	//}
-
 	class System final
 	{
 	public:
-		~System()
-		{
-			for (ComponentInfo& info : Components)
-			{
-				for (IComponent*& pC : info.Components)
-				{
-					delete pC;
-					pC = nullptr;
-				}
-			}
-		}
+		~System();
 
-		Entity CreateEntity()
-		{
-			return Entities.emplace_back(static_cast<Entity>(Entities.size()));
-		}
+#ifdef DEBUG
+		Entity CreateEntity();
+#else
+		__forceinline Entity CreateEntity() { return Entities.emplace_back(static_cast<Entity>(Entities.size())); }
+#endif
 
 		template<typename Component>
-		void AddComponent(const Entity& entity)
-		{
-			assert(entity != InvalidEntityID);
+		void AddComponent(const Entity& entity);
 
-			const ComponentType componentID{ Component::GetComponentID() };
-			ComponentInfo& key{ Components[componentID] };
-
-			if (key.ComponentID == InvalidComponentID)
-			{
-				Components[componentID].ComponentID = componentID;
-			}
-
-			if (static_cast<size_t>(entity) >= key.Components.size())
-			{
-				key.Components.resize(key.Components.size() + entity + 1);
-			}
-
-			key.Components[entity] = new Component{};
-		}
-
+#ifdef DEBUG
 		template<typename Component>
-		Component* GetComponent(const Entity& id) const noexcept
-		{
-			assert(id != InvalidEntityID);
-			assert(Components[Component::GetComponentID()].ComponentID != InvalidComponentID);
-			assert(static_cast<size_t>(id) <= Components[Component::GetComponentID()].Components.size());
-
-			return static_cast<Component*>(Components[Component::GetComponentID()].Components[id]);
-		}
+		Component* const GetComponent(const Entity& id) const noexcept;
+#else
+		template<typename Component>
+		__forceinline Component* const GetComponent(const Entity& id) const noexcept { return static_cast<Component*>(Components[Component::GetComponentID()].Components[id]); }
+#endif
 
 		template<typename ... Components>
-		constexpr std::array<ComponentType, sizeof...(Components)> GetComponents() const noexcept
-		{
-			return { (GetComponentInfo<Components>(), ...) };
-		}
-
-		__forceinline const std::vector<Entity>& GetEntities() const noexcept { return Entities; }
-		__forceinline std::vector<Entity>& GetEntities() noexcept { return Entities; }
+		__forceinline std::array<ComponentType, sizeof...(Components)> GetComponents() const noexcept { return { (GetComponentInfo<Components>(), ...) }; }
+		__forceinline const auto& GetEntities() const noexcept { return Entities; }
+		__forceinline auto& GetEntities() noexcept { return Entities; }
 
 	private:
+#ifdef DEBUG
 		template<typename Component>
-		__forceinline ComponentType GetComponentInfo() const noexcept
-		{
-			assert(Components[Component::GetComponentID()].ComponentID != InvalidComponentID);
-
-			return Components[Component::GetComponentID()].ComponentID;
-		}
+		ComponentType GetComponentInfo() const noexcept;
+#else
+		template<typename Component>
+		__forceinline ComponentType GetComponentInfo() const noexcept { Components[Component::GetComponentID()].ComponentID; }
+#endif
 
 		struct ComponentInfo final
 		{
@@ -134,4 +55,45 @@ namespace ECS
 		std::vector<Entity> Entities;
 		std::array<ComponentInfo, MaxComponentTypes> Components;
 	};
+	
+	template<typename Component>
+	void System::AddComponent(const Entity& entity)
+	{
+		assert(entity != InvalidEntityID);
+
+		const ComponentType componentID{ Component::GetComponentID() };
+		ComponentInfo& key{ Components[componentID] };
+
+		if (key.ComponentID == InvalidComponentID)
+		{
+			Components[componentID].ComponentID = componentID;
+		}
+
+		if (static_cast<size_t>(entity) >= key.Components.size())
+		{
+			key.Components.resize(key.Components.size() + entity + 1);
+		}
+
+		key.Components[entity] = new Component{};
+	}
+	
+#ifdef DEBUG
+	template<typename Component>
+	Component* const System::GetComponent(const Entity& id) const noexcept
+	{
+		assert(id != InvalidEntityID);
+		assert(Components[Component::GetComponentID()].ComponentID != InvalidComponentID);
+		assert(static_cast<size_t>(id) <= Components[Component::GetComponentID()].Components.size());
+
+		return static_cast<Component*>(Components[Component::GetComponentID()].Components[id]);
+	}
+	
+	template<typename Component>
+	ComponentType System::GetComponentInfo() const noexcept
+	{
+		assert(Components[Component::GetComponentID()].ComponentID != InvalidComponentID);
+
+		return Components[Component::GetComponentID()].ComponentID;
+	}
+#endif
 }
