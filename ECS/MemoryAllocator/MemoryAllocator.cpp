11ce8edeed348b2653401ebad4d0783b7538ea47
Rhidian De Wit <rhidiandewit@gmail.com>
2022-04-15 23:59:10 +0200

    made GetFreeBlock()

--- ECS/MemoryAllocator/MemoryAllocator.cpp
+++ ECS/MemoryAllocator/MemoryAllocator.cpp
@@ -1,6 +1,30 @@
 #include "MemoryAllocator.h"
 
+#include <assert.h>
+
 namespace ECS
 {
+    void* MemoryAllocator::allocate(size_t size) 
+    {
+        assert(size != 0);
 
+
+    }
+
+    BlockInformation* MemoryAllocator::GetFreeBlock(size_t size) const
+    {
+        BlockInformation* pCurrent{ Head };
+
+        while (pCurrent)
+        {
+            if (pCurrent->IsFree && pCurrent->BlockSize >= size)
+            {
+                return pCurrent;
+            }
+
+            pCurrent = pCurrent->pNext;
+        }
+
+        return nullptr;
+    }
 }
--- ECS/MemoryAllocator/MemoryAllocator.h
+++ ECS/MemoryAllocator/MemoryAllocator.h
@@ -2,5 +2,22 @@
 
 namespace ECS
 {
+	struct alignas(16) BlockInformation final
+	{
+		size_t BlockSize;
+		bool IsFree;
+		BlockInformation* pNext;
+	};
 
+	class MemoryAllocator final
+	{
+	public:
+		void* allocate(size_t size);
+
+	private:
+		BlockInformation* GetFreeBlock(size_t size) const;
+
+		BlockInformation* Head;
+		BlockInformation* Tail;
+	};
 }
