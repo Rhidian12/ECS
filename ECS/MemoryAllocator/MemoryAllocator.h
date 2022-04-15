3c30904ec7c0ea10e711f4a38e85c00b040d8b39
Rhidian De Wit <74491212+Rhidian12@users.noreply.github.com>
2022-04-15 11:32:01 +0200

    made allocate()

--- ECS/MemoryAllocator/MemoryAllocator.h
+++ ECS/MemoryAllocator/MemoryAllocator.h
@@ -1,8 +1,26 @@
 #pragma once
 
+#include <assert.h>
+#include <limits>
+
 namespace ECS
 {
-	struct MemoryAllocator
+	template<typename Type>
+	struct MemoryAllocator final
 	{
+		using value_type = Type;
+
+		inline static constexpr SizeOfType = sizeof(Type);
+
+		[[nodiscard]] Type* allocate(size_t sizeToAllocate);
 	};
+
+	template<typename Type>
+	Type* MemoryAllocator<Type>::allocate(size_t sizeToAllocate)
+	{
+		assert(sizeToAllocate <= (std::numeric_limits<size_t>::max() / SizeOfType));
+
+		return static_cast<Type*>(malloc(sizeToAllocate * SizeOfType));
+	}
+
 }
