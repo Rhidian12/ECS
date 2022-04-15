4c7d9b84861f58f99090b10abb10c03c2edfb57a
Rhidian De Wit <rhidiandewit@gmail.com>
2022-04-16 00:21:41 +0200

    made class templated

--- ECS/MemoryAllocator/MemoryAllocator.h
+++ ECS/MemoryAllocator/MemoryAllocator.h
@@ -1,5 +1,9 @@
 #pragma once
 
+#include <assert.h> /* assert() */
+#include <cstdlib> /* malloc() */
+
+/* Based on the following article: */
 /* https://arjunsreedharan.org/ */
 
 namespace ECS
@@ -11,6 +15,7 @@ namespace ECS
 		BlockInformation* pNext;
 	};
 
+	template<typename Type>
 	class MemoryAllocator final
 	{
 	public:
@@ -25,4 +30,88 @@ namespace ECS
 		BlockInformation* Head;
 		BlockInformation* Tail;
 	};
+
+	template<typename Type>
+	MemoryAllocator<Type>::~MemoryAllocator()
+	{
+		BlockInformation* pTemp{ Head };
+
+		while (pTemp)
+		{
+			BlockInformation* pNext{ pTemp->pNext };
+
+			free(pTemp);
+
+			pTemp = pNext;
+		}
+	}
+
+	template<typename Type>
+	void* MemoryAllocator<Type>::allocate(size_t size)
+	{
+		assert(size != 0);
+
+		BlockInformation* pBlockInfo{ GetFreeBlock(size) };
+
+		if (pBlockInfo)
+		{
+			pBlockInfo->IsFree = false;
+			return static_cast<void*>(pBlockInfo + 1);
+		}
+
+		const size_t totalSize{ sizeof(BlockInformation) + size };
+		void* const pBlock{ malloc(totalSize) };
+
+		assert(pBlock);
+
+		pBlockInfo = static_cast<BlockInformation*>(pBlock);
+		pBlockInfo->BlockSize = size;
+		pBlockInfo->IsFree = false;
+		pBlockInfo->pNext = nullptr;
+
+		if (!Head)
+		{
+			Head = pBlockInfo;
+		}
+
+		if (Tail)
+		{
+			Tail->pNext = pBlockInfo;
+		}
+
+		Tail = pBlockInfo;
+
+		return static_cast<void*>(pBlockInfo + 1);
+	}
+	
+	template<typename Type>
+	void MemoryAllocator<Type>::deallocate(void* pBlock)
+	{
+		assert(pBlock);
+
+		BlockInformation* pBlockInfo{};
+		BlockInformation* pTemp{};
+
+		pBlockInfo = static_cast<BlockInformation*>(pBlock) - 1;
+
+		pBlockInfo->IsFree = true;
+	}
+
+	template<typename Type>
+	BlockInformation* MemoryAllocator<Type>::GetFreeBlock(size_t size) const
+	{
+		BlockInformation* pCurrent{ Head };
+
+		while (pCurrent)
+		{
+			if (pCurrent->IsFree && pCurrent->BlockSize >= size)
+			{
+				return pCurrent;
+			}
+
+			pCurrent = pCurrent->pNext;
+		}
+
+		return nullptr;
+	}
 }
