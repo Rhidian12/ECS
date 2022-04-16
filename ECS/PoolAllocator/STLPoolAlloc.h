3aa71b44aab2845c2d51dd0e9a2f6767584b75b7
Rhidian De Wit <74491212+Rhidian12@users.noreply.github.com>
2022-04-16 23:47:51 +0200

    added STL using directives

--- ECS/PoolAllocator/STLPoolAlloc.h
+++ ECS/PoolAllocator/STLPoolAlloc.h
@@ -1,3 +1,32 @@
 #pragma once
 
 #include "PoolAllocator.h"
+
+namespace ECS
+{
+	template<typename Type>
+	class STLPoolAlloc final : private PoolAllocator<Type>
+	{
+	public:
+		/* STL using directives */
+
+		using value_type = Type;
+		using reference = Type&;
+		using const_reference = const Type&;
+		using pointer = Type*;
+		using const_pointer = const Type*;
+		using difference_type = std::ptrdiff_t;
+		using size_type = size_t;
+
+		template<typename OtherType>
+		struct rebind
+		{
+			using other = STLPoolAlloc<OtherType>;
+		};
+
+
+
+	private:
+		STLPoolAlloc* CopiedAllocator;
+	};
+}
