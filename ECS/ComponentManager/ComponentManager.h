8339ba5d8d847d31e9c531f746a7c2c0c1215282
Rhidian De Wit <rhidiandewit@gmail.com>
2022-05-04 14:42:56 +0200

    finished componentmanager

--- ECS/ComponentManager/ComponentManager.h
+++ ECS/ComponentManager/ComponentManager.h
@@ -1,8 +1,103 @@
 #pragma once
 
+#include "../ECSConstants.h"
+
+#include <vector>
+#include <memory>
+#include <assert.h>
+
 namespace ECS
 {
-	class ComponentManager
+	class IComponentArray
 	{
+	public:
+		virtual ~IComponentArray() = default;
+	};
+
+	template<typename TComponent>
+	class ComponentArray final : public IComponentArray
+	{
+	public:
+		void AddComponent(Entity entity)
+		{
+			static_assert(std::is_default_constructible_v<TComponent>, "ComponentArray must be default constructible");
+
+			if (entity > Components.size())
+			{
+				Components.resize(entity + 1);
+			}
+
+			Components[entity] = TComponent();
+		}
+		template<typename ... Values>
+		void AddComponent(Entity entity, Values&&... values)
+		{
+			if (entity > Components.size())
+			{
+				Components.resize(entity + 1);
+			}
+
+			Components[entity] = TComponent(std::forward<Values>(values)...);
+		}
+
+		TComponent& GetComponent(Entity entity) { assert(entity < Components.size()); return Components[entity]; }
+		const TComponent& GetComponent(Entity entity) const { assert(entity < Components.size()); return Components[entity]; }
+
+	private:
+		std::vector<TComponent> Components;
+	};
+
+	class ComponentManager final
+	{
+	public:
+		ComponentManager(const ComponentManager&) noexcept = delete;
+		ComponentManager(ComponentManager&&) noexcept = delete;
+		ComponentManager& operator=(const ComponentManager&) noexcept = delete;
+		ComponentManager& operator=(ComponentManager&&) noexcept = delete;
+
+		template<typename TComponent>
+		void AddComponent(Entity entity)
+		{
+			const ComponentType componentID{ TComponent::GetComponentID() };
+
+			if (componentID > ComponentArrays.size())
+			{
+				ComponentArrays.resize(componentID + 1);
+			}
+
+			std::unique_ptr<IComponentArray>& pArray(ComponentArrays[componentID]);
+			if (!pArray)
+			{
+				pArray = std::make_unique<ComponentArray<TComponent>>();
+			}
+
+			std::static_pointer_cast<ComponentArray<TComponent>>(pArray)->AddComponent(entity);
+		}
+		template<typename TComponent, typename ... Values>
+		void AddComponent(Entity entity, Values&&... values)
+		{
+			const ComponentType componentID{ TComponent::GetComponentID() };
+
+			if (componentID > ComponentArrays.size())
+			{
+				ComponentArrays.resize(componentID + 1);
+			}
+
+			std::unique_ptr<IComponentArray>& pArray(ComponentArrays[componentID]);
+			if (!pArray)
+			{
+				pArray = std::make_unique<ComponentArray<TComponent>>();
+			}
+
+			std::static_pointer_cast<ComponentArray<TComponent>>(pArray)->AddComponent(entity, values...);
+		}
+
+		template<typename TComponent>
+		TComponent& GetComponent(Entity entity) { assert(TComponent::GetComponentID() < ComponentArrays.size()); return std::static_pointer_cast<ComponentArray<TComponent>>(ComponentArrays[TComponent::GetComponentID()])->GetComponent(entity); }
+		template<typename TComponent>
+		const TComponent& GetComponent(Entity entity) const { assert(TComponent::GetComponentID() < ComponentArrays.size()); return std::static_pointer_cast<ComponentArray<TComponent>>(ComponentArrays[TComponent::GetComponentID()])->GetComponent(entity); }
+
+	private:
+		std::vector<std::unique_ptr<IComponentArray>> ComponentArrays;
 	};
 }
