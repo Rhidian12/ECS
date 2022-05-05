bb0ff5d289cee7bb502e4eaaf6de025a8e86f309
Rhidian De Wit <rhidiandewit@gmail.com>
2022-05-05 16:26:19 +0200

    added random floats to point2f

--- ECS/Point2f/Point2f.h
+++ ECS/Point2f/Point2f.h
@@ -1,11 +1,13 @@
 #pragma once
 
+#include "../Utils/Utils.h"
+
 struct Point2f
 {
 	float x, y;
 
 	static Point2f CreateRandomPoint2f(float min, float max)
 	{
-		return Point2f{ RandomFloat(min, max), RandomFloat(min, max) };
+		return Point2f{ ECS::Utils::RandomFloat(min, max), ECS::Utils::RandomFloat(min, max) };
 	}
 };
