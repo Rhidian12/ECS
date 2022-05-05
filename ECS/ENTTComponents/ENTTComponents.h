0a9c017e96c623ff7e5a4d3bad560c78d050b04d
Rhidian De Wit <rhidiandewit@gmail.com>
2022-05-05 16:29:28 +0200

    moved ENTTComponents to their own file

---
+++ ECS/ENTTComponents/ENTTComponents.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "../Point2f/Point2f.h"
+#include "../Utils/Utils.h"
+
+struct ENTTGravity final
+{
+	float Gravity{ -981.f };
+};
+
+struct ENTTTransformComponent final
+{
+	Point2f Position{ Point2f::CreateRandomPoint2f(0.f, 1000.f) };
+	Point2f Rotation{ Point2f::CreateRandomPoint2f(0.f, 360.f) };
+	Point2f Scale{ Point2f::CreateRandomPoint2f(0.f, 3.f) };
+};
+
+struct ENTTRigidBodyComponent final
+{
+	float Mass{ ECS::Utils::RandomFloat(0.f, 100.f) };
+	Point2f Velocity{};
+};
