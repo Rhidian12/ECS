977b39d5b6126875e40236b780d2e71b63376551
Rhidian De Wit <74491212+Rhidian12@users.noreply.github.com>
2022-05-07 15:16:43 +0200

    started work on sparse set

--- ECS/SparseSet/SparseSet.h
+++ ECS/SparseSet/SparseSet.h
@@ -1,6 +1,47 @@
 #pragma once
 
+#include <vector> /* std::vector */
+#include <assert.h> /* assert() */
+
 namespace ECS
 {
+	template<typename SparseValue>
+	class SparseSet final
+	{
+	public:
+		void Add(const SparseValue& value)
+		{
+			PackedSet.push_back(value);
 
+			if (value >= SparseSet.size())
+			{
+				SparseSet.resize(value + 1);
+			}
+
+			SparseSet[value] = Size++;
+		}
+		void Add(SparseValue&& value)
+		{
+			PackedSet.push_back(value);
+
+			if (value >= SparseSet.size())
+			{
+				SparseSet.resize(value + 1);
+			}
+
+			SparseSet[value] = Size++;
+		}
+
+		SparseValue& Find(const SparseValue& value)
+		{
+			assert(value < PackedSet.size());
+
+			// if ()
+		}
+
+	private:
+		std::vector<SparseValue> SparseSet;
+		std::vector<SparseValue> PackedSet;
+		size_t Size;
+	};
 }
