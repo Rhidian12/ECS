0631ef0f9922f90b0fc7370ba26353e6a5f4e1e6
Rhidian De Wit <74491212+Rhidian12@users.noreply.github.com>
2022-05-07 17:31:46 +0200

    added iterators to SparseSet

--- ECS/SparseSet/SparseSet.h
+++ ECS/SparseSet/SparseSet.h
@@ -6,6 +6,108 @@
 
 namespace ECS
 {
+	template<typename Type>
+	class RandomIterator final
+	{
+	public:
+		using difference_type = std::ptrdiff_t;
+
+		RandomIterator(Type* const pPointer)
+			: Pointer{ pPointer }
+		{}
+
+		/* Default rule of 5 is good enough */
+		RandomIterator(const RandomIterator&) noexcept = default;
+		RandomIterator(RandomIterator&&) noexcept = default;
+		RandomIterator& operator=(const RandomIterator&) noexcept = default;
+		RandomIterator& operator=(RandomIterator&&) noexcept = default;
+
+		RandomIterator& operator+=(difference_type diff) { Pointer += diff; return *this; }
+		RandomIterator& operator-=(difference_type diff) { Pointer -= diff; return *this; }
+
+		Type& operator*() { return *Pointer; }
+		Type* operator->() { return Pointer; }
+		Type& operator[](difference_type diff) { return Pointer[diff]; }
+
+		const Type& operator*() const { return *Pointer; }
+		const Type* operator->() const { return Pointer; }
+		const Type& operator[](difference_type diff) const { return Pointer[diff]; }
+
+		RandomIterator& operator++() { ++Pointer; return *this; }
+		RandomIterator& operator--() { --Pointer; return *this; }
+		RandomIterator& operator++(int) { RandomIterator temp(*this); ++Pointer; return temp; }
+		RandomIterator& operator--(int) { RandomIterator temp(*this); --Pointer; return temp; }
+
+		difference_type operator-(const RandomIterator& other) const { return Pointer - other.Pointer; }
+
+		RandomIterator operator+(difference_type diff) const { return RandomIterator(Pointer + diff); }
+		RandomIterator operator-(difference_type diff) const { return RandomIterator(Pointer - diff); }
+
+		friend RandomIterator operator+(difference_type diff, const RandomIterator& it) { return RandomIterator(diff + it.Pointer); }
+		friend RandomIterator operator-(difference_type diff, const RandomIterator& it) { return RandomIterator(diff - it.Pointer); }
+
+		bool operator==(const RandomIterator& it) const { return Pointer == it.Pointer; };
+		bool operator!=(const RandomIterator& it) const { return Pointer != it.Pointer; };
+
+		bool operator>(const RandomIterator& it) const { return Pointer > it.Pointer; };
+		bool operator<(const RandomIterator& it) const { return Pointer < it.Pointer; };
+
+		bool operator>=(const RandomIterator& it) const { return Pointer >= it.Pointer; };
+		bool operator<=(const RandomIterator& it) const { return Pointer <= it.Pointer; };
+
+	private:
+		Type* Pointer{ nullptr };
+	};
+
+	template<typename Type>
+	class RandomConstIterator final
+	{
+	public:
+		using difference_type = std::ptrdiff_t;
+
+		RandomConstIterator(Type* const pPointer)
+			: Pointer{ pPointer }
+		{}
+
+		/* Default rule of 5 is good enough */
+		RandomConstIterator(const RandomConstIterator&) noexcept = default;
+		RandomConstIterator(RandomConstIterator&&) noexcept = default;
+		RandomConstIterator& operator=(const RandomConstIterator&) noexcept = default;
+		RandomConstIterator& operator=(RandomConstIterator&&) noexcept = default;
+
+		RandomConstIterator& operator+=(difference_type diff) { Pointer += diff; return *this; }
+		RandomConstIterator& operator-=(difference_type diff) { Pointer -= diff; return *this; }
+
+		const Type& operator*() const { return *Pointer; }
+		const Type* operator->() const { return Pointer; }
+		const Type& operator[](difference_type diff) const { return Pointer[diff]; }
+
+		RandomConstIterator& operator++() { ++Pointer; return *this; }
+		RandomConstIterator& operator--() { --Pointer; return *this; }
+		RandomConstIterator& operator++(int) { RandomConstIterator temp(*this); ++Pointer; return temp; }
+		RandomConstIterator& operator--(int) { RandomConstIterator temp(*this); --Pointer; return temp; }
+
+		difference_type operator-(const RandomConstIterator& other) const { return Pointer - other.Pointer; }
+
+		RandomConstIterator operator+(difference_type diff) const { return RandomConstIterator(Pointer + diff); }
+		RandomConstIterator operator-(difference_type diff) const { return RandomConstIterator(Pointer - diff); }
+
+		friend RandomConstIterator operator+(difference_type diff, const RandomConstIterator& it) { return RandomConstIterator(diff + it.Pointer); }
+		friend RandomConstIterator operator-(difference_type diff, const RandomConstIterator& it) { return RandomConstIterator(diff - it.Pointer); }
+
+		bool operator==(const RandomConstIterator& it) const { return Pointer == it.Pointer; };
+		bool operator!=(const RandomConstIterator& it) const { return Pointer != it.Pointer; };
+
+		bool operator>(const RandomConstIterator& it) const { return Pointer > it.Pointer; };
+		bool operator<(const RandomConstIterator& it) const { return Pointer < it.Pointer; };
+
+		bool operator>=(const RandomConstIterator& it) const { return Pointer >= it.Pointer; };
+		bool operator<=(const RandomConstIterator& it) const { return Pointer <= it.Pointer; };
+
+	private:
+		Type* Pointer{ nullptr };
+	};
+
 	template<typename SparseValue>
 	class SparseSet final
 	{
@@ -45,6 +147,15 @@ namespace ECS
 		const SparseValue& Back() const { assert(PackedSet.size() > 0); return PackedSet[Size].first; }
 
 		void Clear() { this->SparseSet.clear(); PackedSet.clear(); }
+
+		RandomIterator<SparseValue> begin() noexcept { return RandomIterator(PackedSet.data()); }
+		RandomConstIterator<SparseValue> begin() const noexcept { return RandomConstIterator(PackedSet.data()); }
+
+		RandomIterator<SparseValue> end() noexcept { return RandomIterator(PackedSet.data() + static_cast<SparseValue>(PackedSet.size())); }
+		RandomConstIterator<SparseValue> end() const noexcept { return RandomConstIterator(PackedSet.data() + static_cast<SparseValue>(PackedSet.size())); }
+
+		RandomConstIterator<SparseValue> cbegin() const noexcept { return RandomConstIterator(PackedSet.data()); }
+		RandomConstIterator<SparseValue> cend() const noexcept { return RandomConstIterator(PackedSet.data() + static_cast<SparseValue>(PackedSet.size())); }
 
 	private:
 		std::vector<std::pair<SparseValue, bool>> SparseSet;
