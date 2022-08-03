3325287c9a0d1cf3891507356b0751b66449aa70
Rhidian De Wit <rhidiandewit@gmail.com>
2022-08-03 09:00:20 +0200

    remove SystemID from Constants

--- ECS/ECSConstants.h
+++ ECS/ECSConstants.h
@@ -7,15 +7,12 @@ namespace ECS
 {
 	using Entity = uint32_t; /* This supports ~4 billion possible entities, seems like enough */
 	using ComponentType = uint8_t; /* This supports 256 possible component types, seems like enough */
-	using SystemID = ComponentType; /* This supports as many systems as there are Components */
 
 	constexpr Entity MaxEntities{ std::numeric_limits<Entity>::max() - 1 };
 	constexpr ComponentType MaxComponentTypes{ std::numeric_limits<ComponentType>::max() -1 };
-	constexpr SystemID MaxSystems{ MaxComponentTypes };
 
 	using EntitySignature = std::bitset<MaxComponentTypes>;
 
 	constexpr Entity InvalidEntityID{ std::numeric_limits<Entity>::max() };
 	constexpr ComponentType InvalidComponentID{ std::numeric_limits<ComponentType>::max() };
-	constexpr SystemID InvalidSystemID{ InvalidComponentID };
 }
