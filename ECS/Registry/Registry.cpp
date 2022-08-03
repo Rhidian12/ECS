3b94d4698bd0f35f88b965e9aee1108b6725cf30
Rhidian De Wit <rhidiandewit@gmail.com>
2022-08-03 08:58:25 +0200

    remove comments in Registry

--- ECS/Registry/Registry.cpp
+++ ECS/Registry/Registry.cpp
@@ -6,35 +6,17 @@ namespace ECS
 {
 	Registry::Registry()
 		: EntitySignatures{}
-		// , EntitySignatureIndices{}
 		, Entities{}
 		, CurrentEntityCounter{}
-	{
-		// Entities.Reserve(MaxEntities);
-
-		// for (Entity i{}; i < MaxEntities; ++i)
-		// {
-		// 	Entities.Add(i);
-		// }
-
-		// EntitySignatureIndices.reserve(MaxEntities);
-		// EntitySignatureIndices.resize(MaxEntities, InvalidEntityID);
-
-		// EntitySignatures.reserve(MaxEntities);
-		// EntitySignatures.resize(MaxEntities, EntitySignature());
-	}
+	{}
 
 	Entity Registry::CreateEntity()
 	{
-		// assert(Entities.Size() > 0);
-		// assert(Entities.Contains(CurrentEntityCounter));
 		assert(CurrentEntityCounter <= MaxEntities);
 
 		const Entity entity(CurrentEntityCounter++);
-		//Entities.Remove(entity);
 		Entities.Add(entity);
 
-		// EntitySignatureIndices[entity] = static_cast<Entity>(CurrentEntityCounter++);
 		EntitySignatures.insert(std::make_pair(entity, EntitySignature{}));
 
 		return entity;
@@ -42,16 +24,12 @@ namespace ECS
 
 	bool Registry::ReleaseEntity(Entity entity)
 	{
-		// if (!Entities.Contains(entity))
 		if (Entities.Contains(entity))
 		{
 			ComponentManager::GetInstance().RemoveAllComponents(entity, GetEntitySignature(entity));
 
-			// EntitySignatures[EntitySignatureIndices[entity]] = EntitySignature();
-			// EntitySignatureIndices[entity] = InvalidEntityID;
 			EntitySignatures.erase(entity);
 
-			//Entities.Add(entity);
 			Entities.Remove(entity);
 
 			--CurrentEntityCounter;
