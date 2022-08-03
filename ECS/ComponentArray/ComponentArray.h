d6e1edb63dea00d1934a8f4906a4125879225159
Rhidian De Wit <rhidiandewit@gmail.com>
2022-08-03 15:39:42 +0200

    implement ComponentArray

--- ECS/ComponentArray/ComponentArray.h
+++ ECS/ComponentArray/ComponentArray.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <vector> /* std::vector */
+
 #include "../ECSConstants.h"
 
 namespace ECS
@@ -12,7 +14,32 @@ namespace ECS
 		virtual void Remove(const Entity entity) = 0;
 	};
 
-	class ComponentArray final
+	template<typename T>
+	class ComponentArray final : public IComponentArray
 	{
+	public:
+		T& AddComponent(const Entity entity)
+		{
+			return Components.emplace_back(T{});
+		}
+		template<typename ... Ts>
+		T& AddComponent(const Entity entity, Ts&& ... args)
+		{
+			return Components.emplace_back(T{ args... });
+		}
+
+		virtual void Remove(const Entity entity) override
+		{
+			Components.erase(Components.begin() + entity);
+		}
+
+		T& GetComponent(const Entity entity) { return Components[entity]; }
+		const T& GetComponent(const Entity entity) const { return Components[entity]; }
+
+		std::vector<T>& GetComponents() { return Components; }
+		const std::vector<T>& GetComponents() const { return Components; }
+
+	private:
+		std::vector<T> Components;
 	};
 }
