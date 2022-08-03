4df1a132d91252e207d61c64ca43e1d0a4fb4697
Rhidian De Wit <rhidiandewit@gmail.com>
2022-08-03 15:38:13 +0200

    implement IComponentArray

--- ECS/ComponentArray/ComponentArray.h
+++ ECS/ComponentArray/ComponentArray.h
@@ -1,7 +1,17 @@
 #pragma once
 
+#include "../ECSConstants.h"
+
 namespace ECS
 {
+	class IComponentArray
+	{
+	public:
+		virtual ~IComponentArray() = default;
+
+		virtual void Remove(const Entity entity) = 0;
+	};
+
 	class ComponentArray final
 	{
 	};
