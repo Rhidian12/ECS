c71a20223b436b6c1193bc91d245a207e0b5c9d5
Rhidian De Wit <rhidiandewit@gmail.com>
2022-08-20 17:18:23 +0200

    implement ctors and dtors

--- ECS/Memory/Memory.cpp
+++ ECS/Memory/Memory.cpp
@@ -1,1 +1,20 @@
 #include "Memory.h"
+
+#include <stdlib.h> /* malloc */
+
+ECS::Allocator::Allocator()
+	: Allocator{ 1024 }
+{
+}
+
+ECS::Allocator::Allocator(const size_t totalSize)
+	: pBuffer{}
+	, StackPointer{}
+{
+	pBuffer = malloc(RoundToNextPowerOfTwo(totalSize));
+}
+
+ECS::Allocator::~Allocator()
+{
+	free(pBuffer);
+}
--- ECS/Memory/Memory.h
+++ ECS/Memory/Memory.h
@@ -2,5 +2,28 @@
 
 namespace ECS
 {
+	class Allocator final
+	{
+	public:
+		Allocator();
+		explicit Allocator(const size_t totalSize);
 
+		~Allocator();
+
+	private:
+		size_t RoundToNextPowerOfTwo(size_t v) const
+		{
+			v--;
+			v |= v >> 1u;
+			v |= v >> 2u;
+			v |= v >> 4u;
+			v |= v >> 8u;
+			v |= v >> 16u;
+			v |= v >> 32u;
+			v++;
+		}
+
+		void* pBuffer;
+		size_t StackPointer;
+	};
 }
